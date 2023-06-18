d733ee723d592a34d00ef4164a50f23078199285
jlooper <jen.looper@gmail.com>
2023-06-18 14:40:17 -0400

    restyling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://astro-blog-template.netlify.app',
+  integrations: [mdx(), svelte()],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord'
+    }
+  }
+});
